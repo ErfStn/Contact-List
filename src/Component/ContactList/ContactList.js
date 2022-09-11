833a836ea3d814402b9d529ca179af8213777f32
Erfan Soltani <Erfstn@gmail.com>
2022-09-11 19:07:25 +0430

    add search box

--- src/Component/ContactList/ContactList.js
+++ src/Component/ContactList/ContactList.js
@@ -5,18 +5,21 @@ import { useEffect, useState } from "react";
 import { deleteContact, getContacts } from "../../services/services";
 const ContactList = () => {
   const [contacts, setContacts] = useState([]);
+  const [allContacts, setAllContacts] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    const fetcjContact = async () => {
-      const { data } = await getContacts();
-      setContacts(data);
-    };
     try {
+      const fetcjContact = async () => {
+        const { data } = await getContacts();
+        setContacts(data);
+        setAllContacts(data);
+      };
       fetcjContact();
     } catch (error) {
       console.log(error);
     }
-  }, [contacts]);
+  }, []);
 
   const deleteContactHandler = async (id) => {
     try {
@@ -27,16 +30,42 @@ const ContactList = () => {
       console.log(error);
     }
   };
-
+  const changeHandler = (e) => {
+    setSearch(e.target.value);
+    const value = e.target.value;
+    if (value !== "") {
+      const filterContacts = allContacts.filter((c) => {
+        return Object.values(c)
+          .join(" ")
+          .toLowerCase()
+          .includes(value.toLowerCase());
+      });
+      setContacts(filterContacts);
+      console.log(filterContacts);
+    } else {
+      setContacts(allContacts)
+    }
+  };
   return (
     <div className="contactList">
       <div className="topNav">
         <h3>Contact List</h3>
+        <input
+          onChange={changeHandler}
+          placeholder="search for..."
+          value={search}
+        />
         <Link to="add">Add</Link>
       </div>
       {contacts ? (
         contacts.map((contact) => {
-          return <Contact contact={contact} onDelete={deleteContactHandler} />;
+          return (
+            <Contact
+              contact={contact}
+              onDelete={deleteContactHandler}
+              key={contact.id}
+            />
+          );
         })
       ) : (
         <p>Loading...</p>
